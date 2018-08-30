b9b3e80f7bbecae19db95fefffb6f5121f9682b0
Jack Hyc <Jacek.Hyc@gmail.com>
2018-08-30 17:09:19 -0500

    FundamentalsOne

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace FirstCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Again World!");
+            // loop from 1 to 5 including 5
+                // for (int i = 1; i <= 255; i++)
+                // {
+                //     Console.WriteLine(i);
+                // }
+                for (int j = 1; j <=100; j++)
+                {
+                    if (j % 3 == 0 && j % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    
+                    else if (j % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                        
+                    }
+                    else if (j % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+
+                }
+            string place = "Coding Dojo";
+            Console.WriteLine($"Hello {place}");
         }
     }
 }
